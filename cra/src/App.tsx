7796818ac57648e12b054a4db1ad3fc2488f8d8b
Sávio Carlos Martins Costa <saviocmc@protonmail.com>
2021-10-18 23:45:08 -0300

    add the 'create-react-app' implementation

---
+++ cra/src/App.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import css from './App.module.css';
+
+export default function App() {
+    const [todoList, setTodoList] = useState<string[]>([]);
+    const [todoInputValue, setTodoInputValue] = useState<string>('');
+
+    const addNewTodo = (event: React.FormEvent) => {
+        event.preventDefault();
+        setTodoList([...todoList, todoInputValue]);
+        setTodoInputValue('');
+    }
+
+    return (
+        <div className={css.App}>
+            <h1>TODO APP - CRA</h1>
+            <ul>
+                {todoList.map((todo, index) =>
+                    <li key={index}>{todo}</li>
+                )}
+            </ul>
+            <form onSubmit={(event) => addNewTodo(event)} >
+                <input
+                    placeholder="add todo..."
+                    value={todoInputValue}
+                    onChange={(event) => setTodoInputValue(event.target.value)}
+                />
+                <button type="submit">
+                    Add
+                </button>
+            </form>
+        </div>
+    )
+}
